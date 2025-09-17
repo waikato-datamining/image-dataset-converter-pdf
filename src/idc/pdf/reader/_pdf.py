7669b4a6835b8c471370643a615128af2cc7384b
Peter Reutemann <fracpete@gmail.com>
2025-09-17 16:00:23 +1200

    fixed has_finished() method

--- src/idc/pdf/reader/_pdf.py
+++ src/idc/pdf/reader/_pdf.py
@@ -135,4 +135,4 @@ class PdfImageReader(Reader, PlaceholderSupporter, DataTypeSupporter):
         :return: True if finished
         :rtype: bool
         """
-        return len(self._inputs) == 0
+        return (self._inputs is not None) and len(self._inputs) == 0
